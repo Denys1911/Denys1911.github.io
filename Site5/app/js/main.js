84b7caf0531290451c86bf77c2258544567e8326
Denys1911 <51700221+Denys1911@users.noreply.github.com>
2019-08-06 15:26:02 +0300

    Update main.js

--- Site5/app/js/main.js
+++ Site5/app/js/main.js
@@ -182,6 +182,6 @@ $(document).ready(function(){
 
     /// *** PRELOADER *** ///
     $(window).on('load', function(){
-        $('.preloader').delay(1000).fadeOut('slow');
+        $('.preloader').fadeOut('slow');
     })
 });
