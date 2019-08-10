c1b303ca2e7f90926f6d51bb9c70c0c9170ee3c3
Denys1911 <51700221+Denys1911@users.noreply.github.com>
2019-08-10 22:13:40 +0300

    Update script.js

--- JS-practise/05_DOM_output-to-page/script.js
+++ JS-practise/05_DOM_output-to-page/script.js
@@ -39,10 +39,10 @@ document.querySelector('input').type = 'range';
 document.querySelector('button').addEventListener('click', () => {
     let print = document.querySelector('link').href;
     if (print === 'https://denys1911.github.io/JS-practise/05_DOM_output-to-page/style2.css') {
-        document.querySelector('link').href == './style3.css';
+        document.querySelector('link').href == 'https://denys1911.github.io/JS-practise/05_DOM_output-to-page/style3.css';
         document.querySelector('button').innerText = 'Return stylezation back';
     } else {
-        document.querySelector('link').href == './style2.css';
+        document.querySelector('link').href == 'https://denys1911.github.io/JS-practise/05_DOM_output-to-page/style2.css';
         document.querySelector('button').innerText = 'Print';
     }
 })
