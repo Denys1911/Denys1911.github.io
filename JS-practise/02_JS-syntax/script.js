80642184bb61bbfb572a6e90d05b9dab01659296
Denys1900 <den.od.1911@gmail.com>
2019-08-10 19:38:41 +0300

    First commit

---
+++ JS-practise/02_JS-syntax/script.js
@@ -0,0 +1,14 @@
+/* own.innerHTML = "privet"; */
+/* document.getElementById("own").innerHTML = 777; */
+/* let a=document.getElementById("own");
+console.log(a);
+a.innerHTML = 456;
+a.innerText = "dfghdggd";
+a.innerHTML = "<h1>HIII</h1>";
+own.innerHTML="PRIVET"; */
+document.getElementById("own").innerHTML="GG WP";
+let a=document.querySelector("#own").innerHTML=5555555;
+let year=1998;
+let yearold= -(year-2019);
+console.log(yearold);
+let c=document.querySelector("#own").innerHTML=yearold;  
