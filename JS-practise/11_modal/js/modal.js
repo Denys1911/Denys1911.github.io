c5a9c3d64590e940f0e0885815e922c4be1f2ed4
Denys1900 <den.od.1911@gmail.com>
2019-08-26 21:59:43 +0300

    bug fixes

--- JS-practise/11_modal/js/modal.js
+++ JS-practise/11_modal/js/modal.js
@@ -1,22 +1,20 @@
-document.querySelectorAll('.modal-open').forEach(function (element) {
-    element.onclick = showModal;
-});
+document.querySelectorAll('.modal-open').forEach(element => element.addEventListener('click', showModal));
 
-document.querySelectorAll('.modal-close').forEach(function (element) {
-    element.onclick = hideModal;
-});
+document.querySelectorAll('.modal-close').forEach(element => element.addEventListener('click', hideModal));
 
-document.querySelector('.modal-wrapper').onclick = hideModal;
+document.querySelector('.modal-wrapper').addEventListener('click', hideModal);
 
 function showModal() {
     let idModal = this.dataset.modal;
     document.querySelector(idModal).classList.remove('hide');
-    document.onkeydown = function(event) {
+    document.querySelector('.modal').classList.remove('hide');
+    document.onkeydown = event => {
         if (event.key == 'Escape') hideModal();
-    }
+    };
 }
 
 function hideModal() {
     document.querySelector('.modal-wrapper').classList.add('hide');
+    document.querySelector('.modal').classList.add('hide');
     document.onkeydown = null;
 }
