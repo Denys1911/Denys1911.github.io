f595b0dab5fb637ac7cd55d3be33dd0f4369f47c
Denys1900 <den.od.1911@gmail.com>
2019-10-03 21:23:44 +0300

    first commit

---
+++ React-projects/Todo/build/precache-manifest.510c5efee201246ff0a5ca25b6287919.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1ce6025841ae07a8a46514f394b77a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "513e75f0751b42b76a47",
+    "url": "/static/css/main.a80a6c47.chunk.css"
+  },
+  {
+    "revision": "88ef087707047244ac20",
+    "url": "/static/js/2.65dd199b.chunk.js"
+  },
+  {
+    "revision": "513e75f0751b42b76a47",
+    "url": "/static/js/main.6d9a592a.chunk.js"
+  },
+  {
+    "revision": "aedf08bfd231bc1f44d7",
+    "url": "/static/js/runtime~main.7677ad42.js"
+  }
+]);
