de6c080074823a1d0a10d7f0d318c1ea02750462
Denys Goloborodko <den.od.1911@gmail.com>
2019-11-06 21:13:59 +0200

    refactored site4

--- My_websites/Site4/app/js/main.js
+++ My_websites/Site4/app/js/main.js
@@ -1,16 +1,14 @@
 $(function(){
-    $('.slider__inner').slick({
+    $('.slider__inner, .news__comments').slick({
         prevArrow: '<button type="button" class="slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-next"></button>',
-        infinite: false
+        infinite: false,
+        draggable: true
     });
-    $('select').styler()
-    $('.news__comments').slick({
-        prevArrow: '<button type="button" class="slick-prev"></button>',
-        nextArrow: '<button type="button" class="slick-next"></button>',
-        infinite: false
-    });
+
+    $('select').styler();
+
     $('.menu__btn').on('click', function() {
         $('.menu ul').slideToggle();
     });
-})
+});
