d34c13104edfa2f9a7e0e3eb359809f4ae7541c8
Denys Goloborodko <den.od.1911@gmail.com>
2019-12-19 21:28:23 +0200

    fully refactored site6

--- My_websites/Site6/gulpfile.js
+++ My_websites/Site6/gulpfile.js
@@ -1,12 +1,13 @@
-let gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    gulpStylelint = require('gulp-stylelint'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
-    
-gulp.task('browser-sync', function() {
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const gulpStylelint = require('gulp-stylelint');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const del = require('del');
+const rename = require('gulp-rename');
+
+gulp.task('browser-sync', () => {
     browserSync.init({
         server: {
             baseDir: "app/"
@@ -14,52 +15,93 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('scss', function(){
-    return gulp.src('app/scss/**/*.scss')
-    .pipe(gulpStylelint({
-        failAfterError: false,
-        reporters: [
-          {formatter: 'string', console: true}
-        ]
-    }))
-    .pipe(sass({outputStyle: "expanded"}))
-    .pipe(autoprefixer({
-        grid: true,
-        overRideBrowsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream:true}))
-})
-
-gulp.task('html', function(){
+gulp.task('html', () => {
     return gulp.src('app/*.html')
-    .pipe(browserSync.reload({stream:true}))
-})
+        .pipe(browserSync.reload({stream: true}));
+});
 
-gulp.task('script', function(){
-    return gulp.src('app/js/**/*.js')
-    .pipe(browserSync.reload({stream:true}))
-})
+gulp.task('scss', () => {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(gulpStylelint({
+            failAfterError: false,
+            reporters: [
+                {formatter: 'string', console: true}
+            ]
+        }))
+        .pipe(sass({outputStyle: "compressed"}))
+        .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            grid: true
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
 
-gulp.task('libs', function(){
+gulp.task('css-libs', () => {
     return gulp.src([
-        './node_modules/jquery/dist/jquery.js',
-        './app/plugins/OwlCarousel2/dist/owl.carousel.js',
-        './app/plugins/jQuery.equalHeights-master/jquery.equalheights.js',
-        './app/plugins/fotorama-4.6.4.dev/fotorama.dev.js',
-        './app/plugins/selectize.js-master/dist/js/standalone/selectize.js'
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/hamburgers/dist/hamburgers.css',
+        'node_modules/mmenu-js/dist/mmenu.css',
+        'app/plugins/OwlCarousel2/dist/assets/owl.carousel.css',
+        'app/plugins/OwlCarousel2/dist/assets/owl.theme.default.css',
+        'app/plugins/selectize.js/dist/css/selectize.css',
+        'app/plugins/fotorama-4.6.4.dev/fotorama.dev.css'
     ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
-    .pipe(browserSync.reload({stream:true}))
-})
+        .pipe(concat('_libs.scss'))
+        .pipe(gulp.dest('app/scss/vendors'))
+        .pipe(browserSync.reload({stream: true}));
+});
 
-gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
-    gulp.watch('app/*.html', gulp.parallel('html'))
-    gulp.watch('app/js/**/*.js', gulp.parallel('script'))
-})
+/// *** STYLELINT AUTOFIX TASK *** ///
+gulp.task('fix', () => {
+    return gulp.src('app/scss/base/_fonts.scss') // here point your scss file( either css if you need) which you need
+    // to fix
+        .pipe(gulpStylelint({
+            failAfterError: false,
+            fix: true
+        }))
+        .pipe(gulp.dest('app/scss/base')); // Destination of your fixed file. If you need to override old, just point
+    // destination in the same folder
+});
+/// *** STYLELINT AUTOFIX TASK *** ///
 
-gulp.task('default', gulp.parallel('scss', 'libs', 'browser-sync', 'watch'))
+gulp.task('js', () => {
+    return gulp.src('app/js/**/*.js')
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('js-libs', () => {
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/mmenu-js/dist/mmenu.js',
+        'app/plugins/OwlCarousel2/dist/owl.carousel.js',
+        'app/plugins/jQuery.equalHeights/jquery.equalheights.js',
+        'app/plugins/selectize.js/dist/js/standalone/selectize.js',
+        'app/plugins/fotorama-4.6.4.dev/fotorama.dev.js'
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(gulp.dest('app/js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('app/*.html', gulp.parallel('html'));
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+    gulp.watch('app/js/**/*.js', gulp.parallel('js'));
+});
+
+gulp.task('build', async () => {
+    gulp.src('app/*.html').pipe(gulp.dest('dist'));
+    gulp.src('app/mail.php').pipe(gulp.dest('dist'));
+    gulp.src('app/css/**/*.css').pipe(gulp.dest('dist/css'));
+    gulp.src('app/js/**/*.js').pipe(gulp.dest('dist/js'));
+    gulp.src('app/images/**/*.*').pipe(gulp.dest('dist/images'));
+    gulp.src('app/fonts/**/*.*').pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('del', async () => {
+    del.sync('dist');
+});
+
+gulp.task('default', gulp.parallel('css-libs', 'scss', 'js-libs', 'browser-sync', 'watch'));
