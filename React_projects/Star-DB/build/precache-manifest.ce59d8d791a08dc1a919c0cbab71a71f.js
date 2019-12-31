ebb4b209708709e2efa3f4af5fd99d836284d392
Denys Goloborodko <den.od.1911@gmail.com>
2019-12-31 14:37:37 +0200

    added builded Star-DB version

---
+++ React_projects/Star-DB/build/precache-manifest.ce59d8d791a08dc1a919c0cbab71a71f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a05f293051da33be073bf88d83303e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f643e4ede975268bb06",
+    "url": "/static/css/main.c8af1cd6.chunk.css"
+  },
+  {
+    "revision": "176035517361b8ccdfe4",
+    "url": "/static/js/2.8da5277b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8da5277b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f643e4ede975268bb06",
+    "url": "/static/js/main.063433fc.chunk.js"
+  },
+  {
+    "revision": "456e0927bffc20872c89",
+    "url": "/static/js/runtime-main.8f554d78.js"
+  }
+]);
