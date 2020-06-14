33c569c0bb6c48ea3c2327a60c17eba7244faab2
Denys Goloborodko <den.od.1911@gmail.com>
2020-06-14 12:29:16 +0300

    Updated build version

---
+++ React_projects/Star-DB/build/precache-manifest.cdf297375745b242df02959ad82d54ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e62205b017da425ca53d87e01d55a93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a296057b8a15c7d9e8d7",
+    "url": "/static/css/main.4c1f0694.chunk.css"
+  },
+  {
+    "revision": "10cf2b662a49101f947d",
+    "url": "/static/js/2.1443570f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.1443570f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a296057b8a15c7d9e8d7",
+    "url": "/static/js/main.2929f45e.chunk.js"
+  },
+  {
+    "revision": "d7769709a854bffcd887",
+    "url": "/static/js/runtime-main.327b76ce.js"
+  }
+]);
